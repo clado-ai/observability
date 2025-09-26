8311a97283f7fa534f7097d311f42042581aaf21
DavidBShan <davidbshan@gmail.com>
2025-09-26 12:45:00 -0700

    setup workg

--- sdk/tests/agent.py
+++ sdk/tests/agent.py
@@ -1,10 +1,12 @@
 from browser_use.agent.service import Agent
 from browser_use.llm import ChatAnthropic
 from dotenv import load_dotenv
+import os
 load_dotenv()
 
 agent = Agent(
     task="Find the number of stars of the browser-use repo",
-    llm=ChatAnthropic(model="claude-sonnet-4-0")
+    llm=ChatAnthropic(model="claude-sonnet-4-0"),
+    cdp_url=os.getenv("WEBSOCKET_DEBUGGER_URL")
 )
 agent.run_sync()
